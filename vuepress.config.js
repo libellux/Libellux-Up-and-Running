1428fcd0f0d7c1a9e776321fa9a264cffe363f8e
Libellux <fredrik@libellux.com>
2021-09-07 19:28:44 +0200

    public path

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   themeConfig: {
     // public file path
-    logo: '/img/72x72.png',
+    logo: '/72x72.png',
     // If you set it in the form of `organization/repository`
     // we will take it as a GitHub repo
     repo: 'libellux/Libellux-Up-and-Running-dev',
