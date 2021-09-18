95d471ff03379d1a0d054068b3e5b67f3692f001
Libellux <fredrik@libellux.com>
2021-09-18 22:28:15 +0200

    darkmode brand clr

--- docs/.vuepress/.temp/pages/ossec/index.html.js
+++ docs/.vuepress/.temp/pages/ossec/index.html.js
@@ -292,12 +292,12 @@ export const data = {
   ],
   "filePathRelative": "ossec/README.md",
   "git": {
-    "updatedTime": 1631993421000,
+    "updatedTime": 1631995907000,
     "contributors": [
       {
         "name": "Libellux",
         "email": "fredrik@libellux.com",
-        "commits": 97
+        "commits": 98
       }
     ]
   }
