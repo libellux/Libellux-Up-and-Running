1f8195b716dc8fd683a3e9d9a69f8f5ad18b979a
Libellux <fredrik@libellux.com>
2021-09-29 16:51:33 +0200

    vuepress-plugin-sitemap2

--- vuepress.config.js
+++ vuepress.config.js
@@ -6,7 +6,15 @@ module.exports = {
         skipWaiting: false,
       },
     ],
-    ['@vuepress/plugin-pwa-popup'],
+    [
+      '@vuepress/plugin-pwa-popup'
+    ],
+    [
+      'vuepress-plugin-sitemap2',
+      {
+        hostname: 'https://www.libellux.dev'
+      },
+    ],
   ],
   title: 'Libellux',
   description: 'Libellux: Up & Running provides documentation on how-to install open-source software from source. The focus is Zero Trust Network to enhance the security for existing applications or install tools to detect and prevent threats.',
