d0134036f8cdb56fd0d25f16bf01141f896b9290
Libellux <fredrik@libellux.com>
2021-12-27 19:19:04 +0100

    clamav-rev-3-dev-3

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -65,12 +65,12 @@ export const data = {
     }
   ],
   "git": {
-    "updatedTime": 1640463913000,
+    "updatedTime": 1640464689000,
     "contributors": [
       {
         "name": "Libellux",
         "email": "fredrik@libellux.com",
-        "commits": 121
+        "commits": 122
       },
       {
         "name": "Fredrik Hilmersson",
