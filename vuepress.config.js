7e546923092182a271016808595f4e79470466db
Libellux <fredrik@libellux.com>
2022-07-23 19:08:36 +0200

    fix algolia

--- vuepress.config.js
+++ vuepress.config.js
@@ -12,7 +12,8 @@ module.exports = {
     }),
     docsearchPlugin({
       // options
-      apiKey: '71b8ffe0d9c402797e6b68d9a2ef636a',
+      apiKey: '374dffc87cc7634d4814d1c936b599d3',
+      appId: 'AS5YOFW2K6',
       indexName: 'libellux',
     }),
     pwaPlugin({
