707d6178e1f34f5ec66a55562229ce6ccbe3ae17
Fredrik Hilmersson <fredrik@libellux.com>
2024-02-27 18:50:51 +0100

    testing

--- vuepress.config.js
+++ vuepress.config.js
@@ -200,16 +200,19 @@ export default defineUserConfig({
   title: 'Libellux',
   description: 'Libellux: Up & Running provides documentation on how-to install open-source software from source. The focus is Zero Trust Network to enhance the security for existing applications or install tools to detect and prevent threats.',
   head: [
-    ['script', { src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", google_ad_client: "ca-pub-3592345228354158", crossorigin: "anonymous" }],
+    //['script', { src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", google_ad_client: "ca-pub-3592345228354158", crossorigin: "anonymous" }],
     ['script', { src: "//cdn.cookie-script.com/s/fa4657f337d83fad06d3aaf87a232c6d.js" }],
+    ['script', { src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" }],
+    ['script', {}, 
+    '(adsbygoogle = window.adsbygoogle || []).push({  google_ad_client: "ca-pub-3592345228354158",  enable_page_level_ads: true });'],
 
-    ['script', {}, `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-5DHC7KR');
-    `],
+//    ['script', {}, `
+//      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//      })(window,document,'script','dataLayer','GTM-5DHC7KR');
+//    `],
 
     // Robots
     ['meta', { name: 'robots', content: 'index, follow' }],
